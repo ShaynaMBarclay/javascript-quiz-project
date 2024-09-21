3e114f5d8f1b06ec22ff49142f4f78a00132a0fb
”Nado <nadya.bouaouich@gmail.com>
2024-09-21 15:44:17 +0200

    Shayna changes

--- src/index.js
+++ src/index.js
@@ -28,13 +28,13 @@ document.addEventListener("DOMContentLoaded", () => {
   const questions = [
     new Question("What is 2 + 2?", ["3", "4", "5", "6"], "4", 1),
     new Question("What is the capital of France?", ["Miami", "Paris", "Oslo", "Rome"], "Paris", 1),
-    new Question("Who created JavaScript?", ["Plato", "Brendan Eich", "Lea Verou", "Bill Gates"], "Brendan Eich", 2),
-    new Question("What is the mass–energy equivalence equation?", ["E = mc^2", "E = m*c^2", "E = m*c^3", "E = m*c"], "E = mc^2", 3),
+    new Question("Which is the most popular cake in France", ["Paris Brest", "Mille Feuille", "Fraisier", "Tarte au Citron"], "Fraisier", 2),
+    new Question("What ancient civilization revered cats, associating them with the goddess Bastet?", ["Greece", "Persia", "Egypt", "Rome"], "Egypt", 3),
     // Add more questions here
   ];
   const quizDuration = 120; // 120 seconds (2 minutes)
 
-
+ß
   /************  QUIZ INSTANCE  ************/
   
   // Create a new Quiz instance object
